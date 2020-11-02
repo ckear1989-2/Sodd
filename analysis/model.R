2a51836d34f60bdb54fa2963320b78ae492f67ed
ckear1989 <ckear1989@gmail.com>
2020-11-02 21:23:45 +0000

    more data

--- analysis/model.R
+++ analysis/model.R
@@ -4,10 +4,32 @@ library('gbm')
 
 set.seed(123)
 a.dt <- readRDS('~/data/R/rds/a.dt.rds')
-train.dt <- a.dt[ddate < as.Date('2020/10/01', '%Y/%m/%d'), ]
-test.dt <- a.dt[ddate >= as.Date('2020/10/01', '%Y/%m/%d'), ]
-model <- gbm(as.formula('gain ~ hometeam + awayteam + ftr + ip'),
-  data=train.dt, distribution="gaussian", train.fraction=0.7, n.trees=500, keep.data=FALSE, verbose=TRUE)
+train.dt <- a.dt[date < as.Date('2020/10/01', '%Y/%m/%d'), ]
+test.dt <- a.dt[date >= as.Date('2020/10/01', '%Y/%m/%d'), ]
+xvar <- c(
+  'hometeam',
+  'awayteam',
+  'div',
+  'ftr',
+  'ip',
+  'hpr',
+  'apr'
+)
+yvar <- 'gain'
+formula <- as.formula(paste(yvar, paste(xvar, collapse="+"), sep="~"))
+model <- gbm(
+  formula=formula,
+  data=train.dt,
+  distribution="gaussian",
+  train.fraction=0.7,
+  n.trees=500,
+  shrinkage=0.01,
+  interaction.depth=3,
+  keep.data=FALSE,
+  verbose=TRUE
+)
+gbm.perf(model, plot.it=FALSE)
+summary(model, plotit=FALSE)
 train.dt[, gbmp := predict(model, train.dt)]
 test.dt[, gbmp := predict(model, test.dt)]
 train.dt[, list(gain=sum(gain), egain=sum(gbmp))]
@@ -16,8 +38,22 @@ train.dt[, gbmp := gbmp * sum(train.dt[, gain]) / sum(train.dt[, gbmp])]
 test.dt[, gbmp := gbmp * sum(train.dt[, gain]) / sum(train.dt[, gbmp])]
 train.dt[, list(gain=sum(gain), egain=sum(gbmp))]
 test.dt[, list(gain=sum(gain), egain=sum(gbmp))]
+summary(train.dt[, list(gain, gbmp)])
+summary(test.dt[, list(gain, gbmp)])
 
 summary(train.dt[, gbmp])
 summary(test.dt[, gbmp])
-test.dt[gbmp>0, ]
+test.dt[gbmp>0, list(date, hometeam, awayteam, ftr, actr, ip, gbmp, gain)][order(-gbmp)]
+test.dt[
+  gbmp>0,
+  list(
+    ftr=as.list(ftr),
+    actr=as.list(actr),
+    ip=as.list(ip),
+    gbmp=as.list(gbmp),
+    gain=as.list(gain)
+  ),
+  by=list(date, hometeam, awayteam)
+]
+
 test.dt[gbmp>0, list(gain=sum(gain), egain=sum(gbmp))]
--- data_prep/standardise.R
+++ data_prep/standardise.R
@@ -10,49 +10,130 @@ alist <- lapply(
     '~/data/1718/E0.csv',
     '~/data/1617/E0.csv',
     '~/data/1516/E0.csv',
-    '~/data/1415/E0.csv'
+    '~/data/1415/E0.csv',
+    '~/data/1314/E0.csv',
+    '~/data/1213/E0.csv',
+    '~/data/1112/E0.csv',
+    '~/data/1011/E0.csv',
+    '~/data/2021/E1.csv',
+    '~/data/1920/E1.csv',
+    '~/data/1819/E1.csv',
+    '~/data/1718/E1.csv',
+    '~/data/1617/E1.csv',
+    '~/data/1516/E1.csv',
+    '~/data/1415/E1.csv',
+    '~/data/1314/E1.csv',
+    '~/data/1213/E1.csv',
+    '~/data/1112/E1.csv',
+    '~/data/1011/E1.csv'
   ),
   fread
 )
 a.dt <- rbindlist(alist, fill=TRUE)
-# head(a.dt)
+a.dt[HomeTeam=="Brentford" & AwayTeam=="Blackburn", ]
 # colnames(a.dt)
+a.dt[Div=='E0', ]
+a.dt[Div=='E1', ]
 setnames(a.dt, colnames(a.dt), tolower(colnames(a.dt)))
-# class(a.dt[, date])
-# a.dt[1:10, date]
-# a.dt[, .N, nchar(date)]
-# a.dt[nchar(date) == 8, date := paste0(strsplit(date, "/")[[1]][[1]], "/", strsplit(date, "/")[[1]][[2]], "/20", strsplit(date, "/")[[1]][[3]]), list(date, hometeam, awayteam)]
 a.dt[nchar(date)==8, ddate := as.Date(date, '%d/%m/%y')]
 a.dt[nchar(date)==10, ddate := as.Date(date, '%d/%m/%Y')]
-# class(a.dt[, date])
-# a.dt[1:10, date]
-# a.dt[is.na(date), .N]
-# a.dt[, list(date, rn)]
-summary(a.dt[, date])
+print(paste0('na date count:', a.dt[is.na(date), .N]))
+a.dt <- a.dt[!is.na(ddate), ]
 summary(a.dt[, ddate])
-a.dt[, list(date, ddate)]
-a.dt <- a.dt[, list(div, date, ddate, time, hometeam, awayteam, ftr, b365h, b365d, b365a)]
+a.dt <- a.dt[, list(div, date=ddate, hometeam, awayteam, ftr, b365h, b365d, b365a)]
+summary(a.dt)
+a.dt[is.na(hometeam), ]
+a.dt[hometeam == '', ]
+print(a.dt[, .N])
+a.dt <- na.omit(a.dt)
+print(a.dt[, .N])
 a.dt[, b365h := 1 / b365h]
 a.dt[, b365d := 1 / b365d]
 a.dt[, b365a := 1 / b365a]
 a.dt[, sip := b365h + b365d + b365a]
 # head(a.dt)
 a.dt <- rbind(
-  a.dt[, list(div, date, ddate, time, hometeam, awayteam, ftr, ip=b365h, act=as.numeric(ftr=='H'))],
-  a.dt[, list(div, date, ddate, time, hometeam, awayteam, ftr, ip=b365d, act=as.numeric(ftr=='D'))],
-  a.dt[, list(div, date, ddate, time, hometeam, awayteam, ftr, ip=b365a, act=as.numeric(ftr=='A'))]
+  a.dt[, list(div, date, hometeam, awayteam, actr=ftr, ip=b365h, act=as.numeric(ftr=='H'), ftr='H')],
+  a.dt[, list(div, date, hometeam, awayteam, actr=ftr, ip=b365d, act=as.numeric(ftr=='D'), ftr='D')],
+  a.dt[, list(div, date, hometeam, awayteam, actr=ftr, ip=b365a, act=as.numeric(ftr=='A'), ftr='A')]
 )
-setkey(a.dt, ddate)
+print(a.dt[, .N])
+a.dt <- na.omit(a.dt)
+print(a.dt[, .N])
+setkey(a.dt, date)
 a.dt[, rn := seq(a.dt[, .N])]
 a.dt[, gain := act - ip]
-# head(a.dt)
-#class(a.dt[, hometeam])
+for(ateam in unique(a.dt[, hometeam])) {
+
+  team.dt <- a.dt[(act == 1) & ((hometeam == ateam) | (awayteam == ateam)), ]
+  team.dt[(hometeam == ateam) & (ftr == 'H'), r := 'W']
+  team.dt[(hometeam == ateam) & (ftr == 'A'), r := 'L']
+  team.dt[(awayteam == ateam) & (ftr == 'H'), r := 'L']
+  team.dt[(awayteam == ateam) & (ftr == 'A'), r := 'W']
+  team.dt[ftr == 'D', r := 'D']
+
+  h.team.dt <- team.dt[hometeam == ateam, list(hometeam, awayteam, date, r)]
+  setkey(h.team.dt, date)
+  h.team.dt[, trn := seq(h.team.dt[, .N])]
+  hl.team.dt <- h.team.dt[, list(hpr=r, trn=trn+1)]
+  setkey(h.team.dt, trn)
+  setkey(hl.team.dt, trn)
+  h.team.dt <- merge(h.team.dt, hl.team.dt, all.x=TRUE, all.y=FALSE)[, list(hometeam, awayteam, date, hpr)]
+
+  a.team.dt <- team.dt[awayteam == ateam, list(hometeam, awayteam, date, r)]
+  setkey(a.team.dt, date)
+  a.team.dt[, trn := seq(a.team.dt[, .N])]
+  al.team.dt <- a.team.dt[, list(apr=r, trn=trn+1)]
+  setkey(a.team.dt, trn)
+  setkey(al.team.dt, trn)
+  a.team.dt <- merge(a.team.dt, al.team.dt, all.x=TRUE, all.y=FALSE)[, list(hometeam, awayteam, date, apr)]
+
+  if(exists("h.teams.dt")) {
+    h.teams.dt <- rbind(h.teams.dt, h.team.dt)
+  } else {
+    h.teams.dt <- h.team.dt
+  }
+  if(exists("a.teams.dt")) {
+    a.teams.dt <- rbind(a.teams.dt, a.team.dt)
+  } else {
+    a.teams.dt <- a.team.dt
+  }
+}
+print(colnames(a.dt,))
+setkey(a.dt, date, hometeam, awayteam)
+print(a.dt[, .N])
+a.dt <- unique(a.dt)
+print(a.dt[, .N])
+print(colnames(h.teams.dt,))
+setkey(h.teams.dt, date, hometeam, awayteam)
+print(h.teams.dt[, .N])
+h.teams.dt <- unique(h.teams.dt)
+print(h.teams.dt[, .N])
+print(colnames(a.teams.dt,))
+setkey(a.teams.dt, date, hometeam, awayteam)
+print(a.teams.dt[, .N])
+a.teams.dt <- unique(a.teams.dt)
+print(a.teams.dt[, .N])
+a.dt <- merge(a.dt, h.teams.dt)
+a.dt <- merge(a.dt, a.teams.dt)
+print(head(a.dt))
+a.dt[, .N, hpr]
+a.dt[, .N, apr]
+# set character variables to factor for modeling
 a.dt[, hometeam := as.factor(hometeam)]
 a.dt[, awayteam := as.factor(awayteam)]
+a.dt[, div := as.factor(ftr)]
 a.dt[, ftr := as.factor(ftr)]
-# a.dt[, list(count=.N, ip=sum(ip), act=sum(act), gain=sum(gain)), ftr]
-# a.dt[, list(count=.N, ip=sum(ip), act=sum(act), gain=sum(gain)), date]
+a.dt[, hpr := as.factor(hpr)]
+a.dt[, apr := as.factor(apr)]
+print(a.dt[, .N])
+a.dt <- na.omit(a.dt)
+print(a.dt[, .N])
+summary(a.dt)
+a.dt[gain == -Inf, ]
+a.dt <- a.dt[gain != -Inf, ]
 
-# dir.create('~/data/R/rds/')
+setkey(a.dt, rn)
+if(!file.exists('~/data/R/rds/')) dir.create('~/data/R/rds/')
 saveRDS(a.dt, '~/data/R/rds/a.dt.rds')
 
